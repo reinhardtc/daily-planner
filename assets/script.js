9473d8690122d707cca8687deb4d6ae725a3adff
reinhardtc <reinhardtc22@gmail.com>
2021-04-20 20:19:40 -0500

    added buttons and event listener

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,19 @@
 var currentDayEl = document.querySelector('#currentDay');
+var saveBtnEl = document.querySelector('.saveBtn');
 
 // function to update current day
 function dateDisplay() {
   currentDayEl.textContent = moment().format('MMM Do YYYY');
 }
+
+// show time blocks in corresponding colors, if they are in the past, present, or future
+
+// make time block editable
+
+// when save icon is clicked, event is saved to local storage
+
+saveBtnEl.addEventListener('click', function () {
+  console.log('the button was clicked');
+});
 
 dateDisplay();
